7f2ad328c65e56014bf59f7ebf036ab0b10f43ea
erfjs <erfan.granmayeh@gmail.com>
2025-03-09 00:14:57 +0330

    server_end-point

--- src/api/formApi.ts
+++ src/api/formApi.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const fetchFormStructure = async () => {
-	const response = await axios.get('/api/insurance/forms');
+	const response = await axios.get('https://assignment.devotel.io/api/insurance/forms');
 	return response.data;
 };
 
 export const submitForm = async (data: any) => {
-	const response = await axios.post('/api/insurance/forms/submit', data);
+	const response = await axios.post('https://assignment.devotel.io/api/insurance/forms/submit', data);
 	return response.data;
 };
--- src/api/submissionApi.ts
+++ src/api/submissionApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchSubmissions = async () => {
-	const response = await axios.get('/api/insurance/forms/submissions');
+	const response = await axios.get('https://assignment.devotel.io/api/insurance/forms/submissions');
 	return response.data;
 };
