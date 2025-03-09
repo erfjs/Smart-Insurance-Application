c8c5b75d41cc1fb6c488deb93359b145eee8d119
erfjs <erfan.granmayeh@gmail.com>
2025-03-10 00:21:34 +0330

    add Content-Type

--- src/api/formApi.ts
+++ src/api/formApi.ts
@@ -1,6 +1,21 @@
 
-import { api } from "./axios";
+// import { api } from "./axios";
 
+
+// export const fetchFormStructure = async () => {
+// 	const response = await api.get("/api/insurance/forms");
+// 	return response.data;
+// };
+
+// export const submitForm = async (data: any) => {
+// 	const response = await api.post("/api/insurance/forms/submit", data);
+// 	return response.data;
+// };
+
+
+import axios from "axios";
+
+export const api = axios.create();
 
 export const fetchFormStructure = async () => {
 	const response = await api.get("/api/insurance/forms");
@@ -8,6 +23,10 @@ export const fetchFormStructure = async () => {
 };
 
 export const submitForm = async (data: any) => {
-	const response = await api.post("/api/insurance/forms/submit", data);
+	const response = await api.post("/api/insurance/forms/submit", {
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(data),
+	});
+
 	return response.data;
 };
