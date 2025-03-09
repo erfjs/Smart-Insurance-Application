9d0685790f579843d0ad06bfa04d171cb10bbebd
erfjs <erfan.granmayeh@gmail.com>
2025-03-09 17:17:21 +0330

    Cors-resolve

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.NODE_ENV === 'development'
 	? 'api'
-	: 'https://assignment.devotel.io';
+	: 'https://assignment.devotel.io/api';
 
 export const api = axios.create({
 	baseURL: API_BASE_URL
